7b7f022911e8e508174f3b65f39ceb17d623328b
Oleg <logosemi@mail.ru>
2024-04-30 19:57:35 +0500

    inition commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int Ticer = 13676;
+
+        int price = 20;{
+            int mile = 1;
+        }
+        int mile = Ticer / price;
+            System.out.println("Вам вернулась " + mile + " милями");
+        System.out.println("Можете потратить на новые путишествия ");
+    }
+}
+
